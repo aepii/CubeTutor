f73f47ef75b961aa8e244c70e259d6728547871d
aepii <simentalma@yahoo.com>
2024-02-12 06:29:49 -0600

    Starting logic for cube moves and tests.

--- cube_moves.py
+++ cube_moves.py
@@ -20,3 +20,34 @@ class CubeMoves:
             raise ValueError("Unsupported type for 'moves'. Must be a str.")
 
         return self
+
+    def execute_moves(self, cube):
+
+        while self.moves:
+            move = self.moves.popleft()
+            self._perform_move(cube, move)
+
+    def _perform_move(self, cube, move):
+
+        faces = {'F', 'B', 'L', 'R', 'U', 'D'}
+        face = move[0]
+
+        try:
+            print("Test")
+        except:
+            raise ValueError("Test")
+            
+
+        if face in faces:
+            if not move.endswith("'") and "2" not in move:
+                self._rotate_cube(cube, face)
+            elif "'" and "2" in move:
+                self._rotate_cube(cube, face, counter_clock_wise=True, double=True)
+            elif "'" and not "2" in move:
+                pass
+        else:
+            raise ValueError("Unsupported type for 'move. Must be in terms of F, B, L, R, U or D.")
+
+
+    @staticmethod
+    def _rotate_cube(cube, face, counter_clock_wise=False, double=False):
--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 from cube import CubeBuilder
+from cube_moves import CubeMoves
 
 myCube = CubeBuilder().Cube
 print(myCube)
+myCubeMoves = CubeMoves().add_moves("R2 L2 L' R")
+print(myCubeMoves.moves)
